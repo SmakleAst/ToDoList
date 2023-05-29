62decb489ec98f72b909fad08447726d7613d3a0
SmakleAst <SmakleLover@yandex.ru>
2023-05-29 23:15:34 +0300

    Creating project structure

---
+++ ToDoList.DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Domain.Enum
+{
+    public enum Priority
+    {
+        NotImportant = 1,
+        Important = 2,
+        VeryImportant = 3
+    }
+}
